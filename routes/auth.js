438fae193eb7f442613664bd9e4a733f9ed134a1
Alejandro Isa <alejandroisa@gmail.com>
2021-05-01 23:24:46 -0300

    Se saca la validacion de inicio de Sesion porque ya se valida en React

--- routes/auth.js
+++ routes/auth.js
@@ -7,16 +7,7 @@ const auth = require("../middleware/auth");
 
 //Iniciar Sesion
 // api/auth
-router.post(
-  "/",
-  [
-    check("email", "Agrega un email válido").isEmail(),
-    check("password", "El password debe ser mínimo de 6 caracteres").isLength({
-      min: 6,
-    }),
-  ],
-  authController.autenticarUsuario
-);
+router.post("/", authController.autenticarUsuario);
 
 //Obtiene informacion del usuario autenticado
 router.get("/", auth, authController.usuarioAutenticado);
